d553ff4566a12eb18582dc9ced20986c038c791f
J.A. Aartsen <jamal.aartsen@hva.nl>
2021-07-11 21:58:31 +0200

    Change layout recyclerview

--- app/src/main/java/com/jamal/bugstar/Constants.kt
+++ app/src/main/java/com/jamal/bugstar/Constants.kt
@@ -1,5 +1,5 @@
 package com.jamal.bugstar
 
 const val BASE_URL = "https://api.stackexchange.com/"
-const val QUESTIONS_ENDPOINT = "2.3/questions?site=stackoverflow"
+const val QUESTIONS_ENDPOINT = "2.3/questions?site=stackoverflow&pagesize=100"
 const val ANSWERS_ENDPOINT = "2.3/answers?site=stackoverflow"
